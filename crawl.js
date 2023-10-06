73beca944ff53cffc45c6bc71172fb57ea2c780a
Kevin Robles <kevinronu@gmail.com>
2023-10-06 17:45:58 -0500

    chore: add crawl single page with error handle

--- crawl.js
+++ crawl.js
@@ -1,5 +1,32 @@
 const { JSDOM } = require("jsdom");
 
+async function crawlPage(currentURL) {
+  console.log(`Actively crawling: ${currentURL}`);
+
+  try {
+    const resp = await fetch(currentURL);
+
+    if (resp.status > 399) {
+      console.log(
+        `Error in fetch with status code: ${resp.status}, on page: ${currentURL}`
+      );
+      return;
+    }
+
+    const contentType = resp.headers.get("Content-Type");
+    if (!contentType.includes("text/html")) {
+      console.log(
+        `Non html response, content type: ${contentType}, on page: ${currentURL}`
+      );
+      return;
+    }
+
+    console.log(await resp.text());
+  } catch (error) {
+    console.log(`Error in fetch: ${error.message}, on page: ${currentURL}`);
+  }
+}
+
 function getURLsFromHTML(htmlBody, baseURL) {
   const urls = [];
   const dom = new JSDOM(htmlBody);
@@ -36,6 +63,7 @@ function normalizeURL(urlString) {
 }
 
 module.exports = {
+  crawlPage,
   normalizeURL,
   getURLsFromHTML,
 };
