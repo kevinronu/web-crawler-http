2412c2ef91adb2993132184322168399ccd79f25
Kevin Robles <kevinronu@gmail.com>
2023-10-06 17:58:45 -0500

    chore: add crawl page recursive working

--- crawl.js
+++ crawl.js
@@ -1,6 +1,21 @@
 const { JSDOM } = require("jsdom");
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL, currentURL, pages) {
+  baseURLObj = new URL(baseURL);
+  currentURLObj = new URL(currentURL);
+
+  if (baseURLObj.hostname !== currentURLObj.hostname) {
+    return pages;
+  }
+
+  const normalizeCurrentURL = normalizeURL(currentURL);
+  if (pages[normalizeCurrentURL] > 0) {
+    pages[normalizeCurrentURL]++;
+    return pages;
+  }
+
+  pages[normalizeCurrentURL] = 1;
+
   console.log(`Actively crawling: ${currentURL}`);
 
   try {
@@ -10,7 +25,7 @@ async function crawlPage(currentURL) {
       console.log(
         `Error in fetch with status code: ${resp.status}, on page: ${currentURL}`
       );
-      return;
+      return pages;
     }
 
     const contentType = resp.headers.get("Content-Type");
@@ -18,13 +33,20 @@ async function crawlPage(currentURL) {
       console.log(
         `Non html response, content type: ${contentType}, on page: ${currentURL}`
       );
-      return;
+      return pages;
     }
 
-    console.log(await resp.text());
+    const htmlBody = await resp.text();
+
+    const nextURLs = getURLsFromHTML(htmlBody, baseURL);
+
+    for (const nextURL of nextURLs) {
+      pages = await crawlPage(baseURL, nextURL, pages);
+    }
   } catch (error) {
     console.log(`Error in fetch: ${error.message}, on page: ${currentURL}`);
   }
+  return pages;
 }
 
 function getURLsFromHTML(htmlBody, baseURL) {
